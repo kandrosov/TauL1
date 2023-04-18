585b1aec1975a13f244f9e839e27186cb260c86c
Valeria D'Amante <valeriadamante@gmail.com>
2023-04-18 18:04:04 +0200

    added genTruth

--- Training/predict.py
+++ Training/predict.py
@@ -58,8 +58,15 @@ def to_hwIso(x, y, w, meta):
 
 hwIso = np.concatenate(list(dataset.batch(300).map(to_hwIso).take(100).as_numpy_iterator()))
 hw_fpr, hw_tpr, hw_threasholds = roc_curve(y, hwIso)
-
-with PdfPages('roc_v2.pdf') as pdf:
+print(f'hw_fpr = {hw_fpr} \nhw_tpr={hw_tpr} \nhw_thresholds={hw_threasholds}')
+for fpr_idx in range(0, len(fpr)):
+  if(abs(fpr[fpr_idx]-hw_fpr[1])<0.001):
+    print(abs(fpr[fpr_idx]-hw_fpr[1]), fpr[fpr_idx], hw_fpr[1], threasholds[fpr_idx])
+#for tpr_idx in range(0, len(tpr)):
+#  if(tpr[tpr_idx]-hw_tpr[1]<0.00000001):
+#    print(tpr[tpr_idx], hw_tpr[1], threasholds[tpr_idx])
+ 
+with PdfPages('plots/roc_v1.pdf') as pdf:
   fig, ax = plt.subplots(1, 1, figsize=(7, 7))
   ax.plot(tpr, fpr, )
   ax.errorbar(hw_tpr, hw_fpr, fmt='o', markersize=2, color='red')
