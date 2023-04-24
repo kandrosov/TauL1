f03b64881a3ad2c201be6f841f88426aacf7d8a3
Valeria D'Amante <valeriadamante@gmail.com>
2023-04-24 17:54:59 +0200

    improved efficiency histogram definition

--- Performances/GetEffHist.py
+++ Performances/GetEffHist.py
@@ -2,7 +2,7 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-
+import argparse
 
 event_vars = [
   'run', 'luminosityBlock', 'event', 'nPV', 'step_idx'
@@ -43,24 +43,31 @@ def get_y_info(x,y,w,meta):
 def get_hw_info(x,y,w,meta):
     return meta[:276, get_index('L1Tau_hwIso')]
 
-def add_prediction(input_idx,var,x_bins, model='model_v4'):
-	model = keras.models.load_model(f'models/{model}')
-	dataset = tf.data.Dataset.load(f'skim_v1_tf_v1/taus_{input_idx}', compression='GZIP')
-	ds_pred = dataset.batch(300).map(to_pred)
-	pred = model.predict(ds_pred)
+def get_tau_info(x,y,w,meta):
+    return meta[:276, get_index('L1Tau_type')]
+
+class TauType:
+  e = 0
+  tau = 2
+  jet = 3
+
+
+
+def add_prediction(dataset,pred, var ,x_bins ,thr,required_type='tau'):
 	var_den_presel = np.concatenate(list(dataset.batch(300).map(get_x_var).as_numpy_iterator()))
 	gen_truth = np.concatenate(list(dataset.batch(300).map(get_y_info).as_numpy_iterator()))
+	tau_type = np.concatenate(list(dataset.batch(300).map(get_tau_info).as_numpy_iterator()))
 	hw_iso = np.concatenate(list(dataset.batch(300).map(get_hw_info).as_numpy_iterator()))
-	print(pred.shape, var_den_presel.shape, gen_truth.shape, hw_iso.shape)
-	all_var = np.vstack((var_den_presel[:], pred[:,0], gen_truth[:,0], hw_iso[:])).T
-	condition1 = all_var[:,2]==1
-	condition2 = all_var[:,1]>0.40430108# 0.6482158
-	condition3 = all_var[:,3]==1
-	condition = condition1 & condition2
-	condition_iso = condition1 & condition3
+	all_var = np.vstack((var_den_presel[:], pred[:,0], gen_truth[:,0], hw_iso[:, 0], tau_type[:,0])).T
+	condition_type = all_var[:,4]==getattr(TauType, required_type)
+	condition_gen_truth = all_var[:,2]==1
+	condition_thr = all_var[:,1]> thr #0.40430108# 0.6482158
+	condition_hwIso = all_var[:,3]==1
+	condition_nn_based =  condition_type & condition_thr
+	condition_cut_based = condition_type & condition_hwIso
 	var_den = all_var[condition1][:,0]
 	var_num = all_var[condition][:,0]
-	var_num_iso = all_var[condition_iso][:,0]
+	var_num_iso = all_car[condition_iso][:,0]
 	print(var_num.min(), var_num.max())
 	print(f"len of {var} num = {len(var_num)}, len of {var} den = {len(var_den)}")
 	import matplotlib.pyplot as plt
@@ -97,9 +104,25 @@ def add_prediction(input_idx,var,x_bins, model='model_v4'):
 	plt.errorbar(bincenter_iso, ratio_iso, yerr=error, fmt='.', color='r')
 
 
-	plt.savefig(f"plots/{var}_efficiency_1.png")
+	plt.savefig(f"plots/{var}_efficiency_{tau_type}.png")
 
 
-var = 'L1Tau_gen_pt'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=False, type=int, default = 2)
+parser.add_argument('--threshold', required=False, type=float, default = 0.40430108)
+parser.add_argument('--model_version', required=False, type=int, default = 7)
+parser.add_argument('--var', required=False, type=str, default = 'L1Tau_gen_pt')
+args = parser.parse_args()
+
+#var = 'L1Tau_gen_pt'
 x_bins=[0,40,60,80,100,150,200,250,300,500]
-add_prediction(2, var, x_bins, 'model_v9')
+
+model = keras.models.load_model(f'models/model_v{args.model_version}')
+dataset = tf.data.Dataset.load(f'skim_v1_tf_v1/taus_{dataset}', compression='GZIP')
+ds_pred = dataset.batch(300).map(to_pred)
+pred = model.predict(ds_pred)
+for tau_type in ['e', 'jet', 'tau']:
+    add_prediction(dataset,pred, args.var ,x_bins ,args.threshold,tau_type)
+
